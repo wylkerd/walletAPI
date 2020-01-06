8ab5e9167ea47c7de3a9b957c8437f7bc65bb224
wylkerd santos <wylkerd@hotmail.com>
2020-01-06 12:32:06 -0300

    Criado UserController e seus testes

---
+++ src/main/java/com/wallet/dto/UserDTO.java
@@ -0,0 +1,46 @@
+package com.wallet.dto;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+	private Long id;
+	@Email(message="Email inválido")
+	private String email;
+	@Length(min=3, max=50, message="O nome deve conter entre 3 e 50 caracteres")
+	private String name;
+	@NotNull
+	@Length(min=6, message="A senha deve conter no minimo 6 caracteres")
+	private String password;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/main/java/com/wallet/response/Response.java
@@ -0,0 +1,29 @@
+package com.wallet.response;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor 
+public class Response<T> {
+  
+	private T data;
+	private List<String> errors;
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public List<String> getErrors() {
+		return errors;
+	}
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
+	
+}
