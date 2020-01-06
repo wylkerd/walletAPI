2e971c77c70581d28aa4d5bca2d8debc376c4d45
wylkerd santos <wylkerd@hotmail.com>
2020-01-06 15:15:14 -0300

    Testes bem sucedidos e propriedades do banco criadas

--- src/main/java/com/wallet/response/Response.java
+++ src/main/java/com/wallet/response/Response.java
@@ -1,5 +1,6 @@
 package com.wallet.response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Getter;
@@ -13,17 +14,22 @@ public class Response<T> {
   
 	private T data;
 	private List<String> errors;
+	
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
+	public List<String> getErrors(){
+		if (this.errors == null) {
+			this.errors = new ArrayList<String>();
+		}
+		return errors;
+	}
 	public T getData() {
 		return data;
 	}
 	public void setData(T data) {
 		this.data = data;
 	}
-	public List<String> getErrors() {
-		return errors;
-	}
-	public void setErrors(List<String> errors) {
-		this.errors = errors;
-	}
+
 	
 }
