6aa30d07b1f3769f6890ad501ba18621aaea7422
wylkerd santos <wylkerd@hotmail.com>
2020-01-07 10:17:57 -0300

    Testes bem sucedidos, senha criptografada e removida do payload

--- src/test/java/com/wallet/HelloWorld.java
+++ src/test/java/com/wallet/HelloWorld.java
@@ -1,19 +1,22 @@
 package com.wallet;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-
+@ActiveProfiles("test")
 public class HelloWorld {
-	
-	@Test 
+
+	@Test
 	public void testHelloWorld() {
-		assertEquals(1,1);
+		assertEquals( 1 , 1);
 	}
+
 }
--- src/test/java/com/wallet/controller/UserControllerTest.java
+++ src/test/java/com/wallet/controller/UserControllerTest.java
@@ -51,7 +51,7 @@ public class UserControllerTest {
 		.andExpect(jsonPath("$.data.id").value(ID))
 		.andExpect(jsonPath("$.data.email").value(EMAIL))
 		.andExpect(jsonPath("$.data.name").value(NAME))
-		.andExpect(jsonPath("$.data.password").value(PASSWORD));
+		.andExpect(jsonPath("$.data.password").doesNotExist());
 	}
 	
 	@Test
